56c5ae47e7330a990a304d6c36e1a4bae884464f
Deepankar <deepankar080892@gmail.com>
2020-05-24 17:37:20 +0200

    Posts service

---
+++ posts/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+    return res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+    const id = randomBytes(4).toString("hex");
+    const { title } = req.body;
+    posts[id] = { id, title };
+    return res.status(201).send(posts[id]);
+});
+
+app.listen(4000, (args) => {
+    console.log("Posts service listening on port 4000");
+});

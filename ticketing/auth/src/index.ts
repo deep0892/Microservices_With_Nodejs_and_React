945b8d83931f6da6b2ba60ebc81f474432ebc377
Deepankar <deepankar080892@gmail.com>
2020-05-31 18:39:11 +0200

    setting up mongodb instance with docker and kubernetes, setting signup and pasword hashing

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -23,6 +24,20 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000!!`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongodb');
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => {
+    console.log(`Server running on port 3000!!`);
+  });
+};
+
+start();
---
+++ ticketing/auth/src/models/user.ts
@@ -0,0 +1,48 @@
+import { Password } from './../services/passowrd';
+import mongoose from 'mongoose';
+
+// An interface that is required to create a user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes he properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };

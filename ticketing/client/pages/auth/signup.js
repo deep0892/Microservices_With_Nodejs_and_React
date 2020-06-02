13191a5deaac5698f169765e5704668bca0b4bea
Deepankar <deepankar080892@gmail.com>
2020-06-02 18:27:40 +0200

    signup form barebone

---
+++ ticketing/client/pages/auth/signup.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <label htmlFor="">Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops.....</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};

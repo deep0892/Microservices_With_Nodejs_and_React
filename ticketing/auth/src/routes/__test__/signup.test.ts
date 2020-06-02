072a29f2617eba7bbb8d4098bb07df920676cb70
Deepankar <deepankar080892@gmail.com>
2020-06-02 14:06:24 +0200

    setting up test with jest

---
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns 1 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns 1 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns 1 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  return request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(400);
+});

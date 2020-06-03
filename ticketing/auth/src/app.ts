a2d67b99f859b44472840581795e8fc696417ec3
Deepankar <deepankar080892@gmail.com>
2020-06-04 00:04:21 +0200

    signup route and hook to capture errors

--- ticketing/auth/src/app.ts
+++ ticketing/auth/src/app.ts
@@ -1,18 +1,21 @@
-import express, { json } from 'express';
+import express from 'express';
+
 import 'express-async-errors';
+
 import bodyParser from 'body-parser';
+
 import cookieSession from 'cookie-session';
 
+import { NotFoundError } from './errors/not-found-error.ts';
+import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error.ts';
 
 const app = express();
 app.set('trust proxy', true);
-app.use(json());
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     signed: false,

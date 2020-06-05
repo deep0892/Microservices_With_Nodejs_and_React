c7fcc59f005371661fdeba5df95475790e9b0a64
Deepankar <deepankar080892@gmail.com>
2020-06-05 23:45:48 +0200

    fetch all tickets api

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -6,6 +6,7 @@ import { NotFoundError, errorHandler, currentUser } from '@dipstickets/common';
 
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,6 +21,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
